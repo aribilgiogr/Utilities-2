db436aeea288c0c7a78091228020af05e9926e3c
aribilgiogr <aribilgiogr@gmail.com>
2025-06-25 10:24:40 +0300

    update 01

--- Utilities/Helpers/TagHelpers/EmailTagHelper.cs
+++ Utilities/Helpers/TagHelpers/EmailTagHelper.cs
@@ -7,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace Utilities.Helpers.TagHelpers
 {
-    public class EmailTagHelper:TagHelper
+    [HtmlTargetElement("email")]
+    public class EmailTagHelper : TagHelper
     {
+        public string Address { get; set; }
+        public string? Display { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href", $"mailto:{Address}");
+            output.Content.SetContent(Display ?? Address);
+        }
     }
 }
